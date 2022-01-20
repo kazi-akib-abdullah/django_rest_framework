fe86b334f8784a8786e63732fe915837dbf5b555
kazi-akib-abdullah <q.aqib.abdullah@gmail.com>
2022-01-21 02:34:05 +0600

    done all

--- status/urls.py
+++ status/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import StatusApiView, StatusListAPIView, StatusCreateAPIView, StatusDetailAPIView, StatusUpdateAPIView, StatusDestroyAPIView
+from .views import StatusViewset
+from rest_framework import routers
+
+# status/ -> List, Create => GET, POST
+# status/<id>/ -> Details, Delete, Update => GET, DELETE, PUT/PATCH
+router = routers.DefaultRouter()
+router.register(r'status', StatusViewset, basename="status")
 urlpatterns = [
-    path('status/', StatusApiView.as_view()),
-    path('status/list/', StatusListAPIView.as_view()),
-    path('status/create/', StatusCreateAPIView.as_view()),
-    path('status/detail/<id>', StatusDetailAPIView.as_view()),
-    path('status/update/<id>', StatusUpdateAPIView.as_view()),
-    path('status/delete/<id>', StatusDestroyAPIView.as_view()),
-]
+    # path('status/', StatusApiView.as_view()),
+    # path('status/', StatusListAPIView.as_view()),
+    # path('status/create/', StatusCreateAPIView.as_view()),
+    # path('status/<id>/', StatusDetailAPIView.as_view()),
+    # path('status/update/<id>/', StatusUpdateAPIView.as_view()),
+    # path('status/delete/<id>/', StatusDestroyAPIView.as_view()),
+]+ router.urls
--- status/views.py
+++ status/views.py
@@ -1,44 +1,69 @@
 # from django.shortcuts import render
-from cgitb import lookup
 from .models import Status
 from .serializers import StatusSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
-
+from rest_framework.generics import  mixins,ListCreateAPIView,RetrieveUpdateDestroyAPIView, ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework import parsers, viewsets
 # Create your views here.
 
-class StatusApiView(APIView):
-    def get(self, request, format=None):
-        status_list = Status.objects.all()
-        status_serializer = StatusSerializer(status_list, many=True)
-        return Response(status_serializer.data)
+# class StatusApiView(APIView):
+#     def get(self, request, format=None):
+#         status_list = Status.objects.all()
+#         status_serializer = StatusSerializer(status_list, many=True)
+#         return Response(status_serializer.data)
 
-class StatusListAPIView(ListAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
+# class StatusListAPIView(mixins.CreateModelMixin, ListAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
-class StatusCreateAPIView(CreateAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
+# class StatusCreateAPIView(CreateAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
 
-class StatusDetailAPIView(RetrieveAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-    lookup_field = 'id'
+# class StatusDetailAPIView(mixins.UpdateModelMixin, mixins.DestroyModelMixin,RetrieveAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     lookup_field = 'id'
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
 
     # def get_object(self, *args, **kwargs):
     #     kwargs = self.kwargs
     #     kw_id = kwargs.get("id")
     #     return Status.objects.get(id=kw_id)
 
-class StatusUpdateAPIView(UpdateAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-    lookup_field = 'id'
+# class StatusUpdateAPIView(UpdateAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     lookup_field = 'id'
 
-class StatusDestroyAPIView(DestroyAPIView):
+# class StatusDestroyAPIView(DestroyAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     lookup_field = 'id'
+
+
+# class StatusListAPIView(ListCreateAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+
+# class StatusDetailAPIView(RetrieveUpdateDestroyAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     lookup_field = 'id'
+#     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+
+
+class StatusViewset(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
-    lookup_field = 'id'
+    parser_classes = [parsers.FormParser, parsers.MultiPartParser]
+
